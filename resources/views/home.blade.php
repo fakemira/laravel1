413a50f0f859bfa5dc086a8b7f5ed5a229334794
Your Name <meer.800454@spu.edu.iq>
2022-04-06 11:39:10 -0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<h1>Home Page</h1>
+<h2>Click <a href="profile">here </a>to visit my profile page</h2>
+<h2>Click <a href="profile">here </a>to see my biography</h2>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,9 @@
+<h1>Mir Ahmad</h1>
+<img style="height: 350; width: 270; border-radius: 10%;" src="picture.jpg">
+<h2>About<h2>
+<p>I’m studying Information Technology at TCI.</p>
+<p>I'm general representative of IT students at TCI.</p>
+<p>I &hearts; programming, worked with C and Python.<p>
+<h2>Contact Details<h2>
+<p>email: dreamilyspace@gmail.com</p>
+<p>phone_number: 0771 153 30 82</p>
